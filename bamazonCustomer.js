74bddf2a4a4338960cbc218b93dbff80a9b81ff0
benohhh <b3njaminoh@gmail.com>
2018-11-05 20:53:18 -0800

    got it working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,41 +3,56 @@ const inq = require('inquirer')
 
 // creates connection 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: 'localhost',
   port: 3306,
-  user: "root",
-  password: "rootroot",
-  database: "bamazon_db"
+  user: 'root',
+  password: 'rootroot',
+  database: 'bamazon_db'
 })
 
-connection.connect(function (err) {
+connection.connect( function (err) {
   if (err) throw err
+  start()
 })
 
 let productsList = []
 
 // will display all products available
-function start () {
-  connection.query('SELECT * FROM products', (err, res) => {
-    if (err) throw err
-    console.log('Current inventory: \n')
-    for (let i = 0; i < res.length; i++)
-    console.log(
-      'Item ID: ' + res[i].item_id + '\n' +
-      'Product name: ' + res[i].product_name + '\n' +
-      'Department: ' + res[i].department_name + '\n' +
-      'Price: ' + res[i].price + '\n' + 
-      'Quantity: ' + res[i].stock_quantity + '\n'
-    )
+function start() {
+  connection.query("SELECT * FROM products", (err, res) => {
+    if (err) throw err;
+    console.log('Current Inventory: \n');
+    for (let i = 0; i < res.length; i++) {
+      console.log(
+        'Item ID: ' + res[i].item_id + '\n' +
+        'Product Name: ' + res[i].product_name + '\n' +
+        'Department: ' + res[i].department_name + '\n' +
+        'Price: $' + res[i].price + '\n' +
+        '---------------------------------------------\n'
+      )
+      // pushes product id and name into array so they can display
+      productsList.push('Item ID: ' + res[i].item_id + ", " + res[i].product_name)
+    }
+    purchase(productsList)
   })
 }
 
-function purchase() {
+function purchase(productsList) {
+  // will ask user what they want to buy
   inq.prompt([
     {
       type: 'list',
       message: 'What would you like to purchase?',
       name: 'choice',
-      choices: 
+      choices: productsList
+    },
+    // will ask user how many they want
+    {
+      name: 'quantity',
+      type: 'input',
+      message: 'Enter quantity'
     }
+  ])
+  .then(answer => {
+  })
 }
